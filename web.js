1ab9db2acf36ed8e23fafb59992d1b17e55f1e9b
Aji <aji.john@gmail.com>
2013-08-07 00:03:09 +0000

    modified js

--- web.js
+++ web.js
@@ -1,12 +1,25 @@
-var express = require('express');
 
+//modiefied web js
+var  express = require("express");
+var fs = require("fs");
 var app = express.createServer(express.logger());
 
+var outb = "";
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+outb = fs.readFileSync("index.html",'utf-8');
+
+response.end(outb);
+
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
+
+
